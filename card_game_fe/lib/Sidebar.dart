559603c28fbb79c98528a99f629860de15b3bf6d
SADAF MTAHERI <sadaf@SADAFs-MacBook-Pro.local>
2024-08-16 12:13:31 +0100

    setup project

---
+++ card_game_fe/lib/Sidebar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatelessWidget{
+  const Sidebar({Key key})  :  super(key: key)
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container( 
+      width: 250,
+      height: double.infinity,
+      color: Colors.orange[300],
+      child: Column(
+        children: <Widget>[ 
+          SizedBox(height: 20 ),
+          ListTile( 
+            leading: Icon(Icons.Home, color: Colors.amber[50],),
+          )
+
+          
+        ],
+      ))
+  }
+}
