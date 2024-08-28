16a74e10ebaae08d634fac8f69f9d52051b7e50d
Hanad999 <calimaxamed195@gmail.com>
2024-08-28 20:39:45 +0100

    added fuctionalites to board

--- card_game_fe/lib/Game/Board.dart
+++ card_game_fe/lib/Game/Board.dart
@@ -1,28 +1,126 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Board extends StatelessWidget {
+class Board extends StatefulWidget {
   const Board({super.key});
 
   @override
+  State<Board> createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  final Player playerOne = new Player('player1',[
+    new Character(
+        "Superman",
+        50,
+        100,
+        new Ability("Critical Strike",
+            "A very, very big hit that does big damage", "attack", 75, 5),
+        100,
+        "url_1"),
+    new Character(
+        "Batman",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_2"),
+    new Character(
+        "Batman2",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_3"),
+    new Character(
+        "Batman3",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_4"),
+    new Character(
+        "Batman4",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_5"),
+  ], 2000);
+  final Player playerTwo = new Player('Player2',[
+    new Character(
+        "GreenLantern",
+        50,
+        100,
+        new Ability("Critical Strike",
+            "A very, very big hit that does big damage", "attack", 75, 5),
+        100,
+        "url_1"),
+    new Character(
+        "Deadpool",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_2"),
+    new Character(
+        "Wolverine",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_3"),
+    new Character(
+        "blue beetle",
+        40,
+        80,
+        new Ability("Sonic Scream", "Ouch, my ears!", "attack", 60, 4),
+        80,
+        "url_4")
+  ], 1500);
+  final bool isPlayerOne = true;
+  @override
   Widget build(BuildContext context) {
+    final Player activePlayer = isPlayerOne ? playerOne : playerTwo;
+    final Player inactivePlayer = isPlayerOne ? playerTwo : playerOne;
     return DecoratedBox(
-        decoration: BoxDecoration(color: const Color.fromARGB(255, 89, 106, 120)),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('lib/assets/battlepageBackground.jpg'),
+                fit: BoxFit.fitHeight)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
-              children: [Gold()],
+              children: [Credit(inactivePlayer.credit)],
             ),
             OpponentRow(),
-            PlayerRow(),
-            BottomUi()
+            PlayerRow(activePlayer, setState),
+            BottomUi(activePlayer)
           ],
         ));
   }
 }
 
-class Gold extends StatelessWidget {
-  const Gold({super.key});
+// const Map numb1 = {
+// 'name': "Ironman",
+// 'health': 90,
+// 'damage': 50,
+// 'point_cost':90,
+// 'imageURL': 'helowhw',
+// 'ability': {
+// 'name': "Rockets",
+// 'description': "A very, very big ability",
+// 'type': "attack",
+// 'strength': 70,
+// 'ability_cost': 4
+// }
+// };
+
+class Credit extends StatelessWidget {
+  final int creditLeft;
+  const Credit(this.creditLeft);
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +129,7 @@ class Gold extends StatelessWidget {
         color: Colors.brown,
       ),
       child: Text(
-        'Gold:7000',
+        'Credit: ${creditLeft}',
         style: TextStyle(fontSize: 18),
       ),
       padding: EdgeInsets.fromLTRB(12, 6, 12, 6),
@@ -47,7 +145,9 @@ class EmptyPosition extends StatelessWidget {
     return SizedBox(
       width: 120,
       height: 160,
-      child: Placeholder(),
+      child: Container(
+          child: DecoratedBox(
+              decoration: BoxDecoration(border: Border.all(width: 5)))),
     );
   }
 }
@@ -71,73 +171,100 @@ class OpponentRow extends StatelessWidget {
 }
 
 class PlayerRow extends StatelessWidget {
-  const PlayerRow({super.key});
+  final Player player;
+  final Function setState;
+  const PlayerRow(this.player, this.setState);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> cardList = []; //
+    for (var i = 0; i <= 4; i++) {
+      if (player.cardsInPlay[i] is Character) {
+        cardList.add(BigCard(player.cardsInPlay[i]));
+      } else {
+        cardList.add(EmptyPositionTarget(i, player, setState));
+      }
+    }
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        EmptyPosition(),
-        EmptyPosition(),
-        EmptyPosition(),
-        EmptyPosition(),
-        EmptyPosition(),
-      ],
+      children: cardList,
     );
   }
 }
 
 class PlayerHand extends StatelessWidget {
-  const PlayerHand({super.key});
+  final List<Character> charactersInHand;
+  const PlayerHand(this.charactersInHand); // removed {super.key}
 
   @override
   Widget build(BuildContext context) {
+    List<CardHandDrag> draggableCards =
+        charactersInHand.map((Character characterCard) {
+      return CardHandDrag(characterCard);
+    }).toList();
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-         SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-         SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-         SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-         SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-        SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        ),
-         SizedBox(
-          height: 100,
-          width: 80,
-          child: Placeholder(),
-        )
-      ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: draggableCards);
+  }
+}
+
+class SmallCard extends StatelessWidget {
+  const SmallCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: 80,
+      child: DecoratedBox(decoration: BoxDecoration(color: Colors.amber)),
     );
   }
 }
 
+class Player {
+  String name;
+  int actionsLeft = 3;
+  int credit;
+  List<Character> cardsInHand;
+  List<Character?> cardsInPlay = [
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  Player(this.name,this.cardsInHand, this.credit);
+}
+
+class Ability {
+  String name;
+  String description;
+  String type;
+  int strength;
+  int ability_cost;
+
+  Ability(
+      this.name, this.description, this.type, this.strength, this.ability_cost);
+}
+
+class Character {
+  String name;
+  int health;
+  int damage;
+  Ability ability;
+  int point_cost;
+  String imageURL;
+
+  Character(this.name, this.health, this.damage, this.ability, this.point_cost,
+      this.imageURL);
+}
+
+
 class InfoBar extends StatelessWidget {
-  const InfoBar({super.key});
+  final Player activePlayer;
+  const InfoBar(this.activePlayer);
 
   @override
   Widget build(BuildContext context) {
@@ -145,8 +272,8 @@ class InfoBar extends StatelessWidget {
         child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Gold(),
-        PlayerTurnInfo(), // to do
+        Credit(activePlayer.credit),
+        PlayerTurnInfo(activePlayer), // to do
         SurrenderBtn(), // to do
         EndTurnBtn() // to do
       ],
@@ -155,7 +282,8 @@ class InfoBar extends StatelessWidget {
 }
 
 class PlayerTurnInfo extends StatelessWidget {
-  const PlayerTurnInfo({super.key});
+  final Player activePlayer;
+  const PlayerTurnInfo(this.activePlayer);
 
   @override
   Widget build(BuildContext context) {
@@ -164,7 +292,7 @@ class PlayerTurnInfo extends StatelessWidget {
         color: const Color.fromARGB(255, 14, 64, 110),
       ),
       child: Text(
-        'Player 1: 2 actions left',
+        '${activePlayer.name}: ${activePlayer.actionsLeft} actions left',
         style: TextStyle(fontSize: 18),
       ),
       padding: EdgeInsets.fromLTRB(12, 6, 12, 6),
@@ -181,9 +309,18 @@ class SurrenderBtn extends StatelessWidget {
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 165, 46, 144),
       ),
-      child: Text(
-        'Surrender!!:(',
-        style: TextStyle(fontSize: 18),
+      child: Container(
+        height: 50,
+        width: 200,
+        child: TextButton(
+          onPressed: () {
+            print('surrender');
+          },
+          child: Text(
+            'Surrender!!:(',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
       ),
       padding: EdgeInsets.fromLTRB(12, 6, 12, 6),
     );
@@ -197,11 +334,20 @@ class EndTurnBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 160, 37, 37),
+        color: const Color.fromARGB(255, 122, 193, 131),
       ),
-      child: Text(
-        'Gold:7000',
-        style: TextStyle(fontSize: 18),
+      child: Container(
+        height: 50,
+        width: 200,
+        child: TextButton(
+          onPressed: () {
+            print('End Turn :(');
+          },
+          child: Text(
+            'End Turn :(',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
       ),
       padding: EdgeInsets.fromLTRB(12, 6, 12, 6),
     );
@@ -209,12 +355,78 @@ class EndTurnBtn extends StatelessWidget {
 }
 
 class BottomUi extends StatelessWidget {
-  const BottomUi({super.key});
+  final Player activePlayer;
+  const BottomUi(this.activePlayer); // remove {super.key}
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [InfoBar(), PlayerHand()],
+      children: [InfoBar(activePlayer), PlayerHand(activePlayer.cardsInHand)],
+    );
+  }
+}
+
+class CardHandDrag extends StatelessWidget {
+  final Character character;
+  const CardHandDrag(this.character); // removed super.key
+
+  @override
+  Widget build(BuildContext context) {
+    return Draggable(
+      data: character,
+      child: SmallCard(),
+      feedback: SmallCard(),
+      childWhenDragging: Container(),
+    );
+  }
+}
+
+class EmptyPositionTarget extends StatelessWidget {
+  final Function setState;
+  final Player player;
+  final int index;
+  const EmptyPositionTarget(this.index, this.player, this.setState);
+  @override
+  Widget build(BuildContext context) {
+    return DragTarget<Character>(
+      builder: (context, candidate, rejected) {
+        return EmptyPosition();
+      },
+      onAcceptWithDetails: (value) {
+        if (player.actionsLeft > 0) {
+          var character = value.data;
+          setState(() {
+            player.cardsInPlay[index] = character;
+            player.cardsInHand = player.cardsInHand.where((character) {
+              return player.cardsInPlay[index] != character;
+            }).toList();
+            print(player.actionsLeft);
+            player.actionsLeft--;
+            print(player.actionsLeft);
+            player.credit -= character.point_cost;
+            print(player.credit);
+          });
+
+          print(player.cardsInPlay);
+        }
+      },
+      onWillAcceptWithDetails: (value) {
+        return true;
+      },
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  final Character? character;
+  const BigCard(this.character);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: 80,
+      child: DecoratedBox(decoration: BoxDecoration(color: Colors.amber)),
     );
   }
 }
--- card_game_fe/lib/Game/Game.dart
+++ card_game_fe/lib/Game/Game.dart
@@ -4,7 +4,6 @@ import 'Board.dart';
 
 class Game extends StatelessWidget {
   const Game({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
